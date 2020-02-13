f1807340f49cca34d87b5328a0cf55d3d327487c
jonathanlurie <lurie.jo@gmail.com>
2020-02-13 16:42:34 +0100

    first
    Change-Id: I84ea2b7e5231f11f27a98a0aa7e4ae03c9d5b8b7

---
+++ README.md
@@ -0,0 +1,16 @@
+# Data Fetch
+This module, in charge of fetching datasets from Nexus and copy them on local storage.
+
+# Install
+```
+cd bluebrainatlas-datafetch
+pip install .
+```
+
+Then, the command `bba-datafetch` becomes available in the system PATH.
+
+# Use
+Display help:
+```
+bba-datafetch --help
+```
---
+++ src/bluebrainatlas_datafetch/main.py
@@ -0,0 +1,183 @@
+"""
+    Download the file attached to a given resource from Nexus.
+    If no file is atatched and the output required is actually the payload,
+    then the output extension must be .json
+"""
+
+import argparse
+import sys
+import os
+import json
+import nexussdk as nexus
+
+from bluebrainatlas_datafetch import __version__
+
+__author__ = "Jonathan Lurie"
+__copyright__ = "EPFL - The Blue Brain Project"
+__license__ = ""
+
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description=__doc__)
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="bluebrainatlas-datafetch {ver}".format(ver=__version__))
+
+    parser.add_argument(
+        "-nexus-token-file",
+        dest="nexus_token_file",
+        required=True,
+        help="Local path to the file containing the Nexus token"
+    )
+
+    parser.add_argument(
+        "-nexus-env",
+        dest="nexus_env",
+        required=True,
+        help="URL to the Nexus environment"
+    )
+
+    parser.add_argument(
+        "-nexus-org",
+        dest="nexus_org",
+        required=True,
+        help="The Nexus organization where the resource is"
+    )
+
+    parser.add_argument(
+        "-nexus-proj",
+        dest="nexus_proj",
+        required=True,
+        help="The Nexus project where the resource is"
+    )
+
+    parser.add_argument(
+        "-nexus-id",
+        dest="nexus_id",
+        required=True,
+        help="The Nexus @id of the resource to fetch the linked file of"
+    )
+
+    parser.add_argument(
+        "-out",
+        dest="out",
+        required=True,
+        help="Output filepath"
+    )
+    
+    parser.add_argument(
+        "-rev",
+        dest="nexus_rev",
+        required=False,
+        default=None,
+        help="OPTIONAL Revision of the Nexus resource"
+    )
+
+    parser.add_argument(
+        "-tag",
+        dest="nexus_tag",
+        required=False,
+        default=None,
+        help="OPTIONAL Tag of the Nexus resource"
+    )
+
+    return parser.parse_args(args)
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    args = parse_args(args)
+
+    # setting Nexus SDK
+    token = open(args.nexus_token_file, 'r').read().strip()
+    nexus.config.set_token(token)
+
+    if args.nexus_env[-1] == '/':
+        args.nexus_env = args.nexus_env[:-1]
+    nexus.config.set_environment(args.nexus_env)
+
+    # Fetching the resource of interest
+    try:
+        resource = nexus.resources.fetch(args.nexus_org, args.nexus_proj, args.nexus_id, rev=args.nexus_rev, tag=args.nexus_tag)
+    except Exception as e:
+        print("❌", e)
+        exit(1)
+
+    output_extension = args.out.split(".").pop().lower()
+
+    # Make sure the parent directory of the specified output exist, if not, create it
+    parent_dir = os.path.dirname(args.out)
+    if not os.path.isdir(parent_dir):
+        os.makedirs(parent_dir)
+
+    if "distribution" in resource and "contentUrl" in resource["distribution"]:
+        linked_file_extension = resource["distribution"]["name"].split(".").pop().lower()
+
+        # check that extension of distant file and the output is the same (case not sensitive)
+        if linked_file_extension != output_extension:
+            print("❌ The output extension is", output_extension, "while the distant file extension is", linked_file_extension, " - They must be the same.")
+            exit(1)
+
+        # fetching the file
+        file_id = resource["distribution"]["contentUrl"].split("/")[-1]
+        nexus.files.fetch(args.nexus_org, args.nexus_proj, file_id , out_filepath=args.out)
+    else:
+
+        # If there is no file attached to the resource, we want to write the payload as a JSON
+        # output file. Though if the extension of the given output is not json, then we raise an error and quit.
+        if output_extension != "json":
+            print("❌ This resource is not linked to any file. The output extension must be .json to output a payload file.")
+            exit(1)
+
+        # removing the file if it exists
+        if os.path.exists(args.out):
+            os.remove(args.out)
+
+        # sanitize from all the Nexus meta
+        resource.pop("@context", None)
+        resource.pop("@id", None)
+        resource.pop("@type", None)
+        resource.pop("_self", None)
+        resource.pop("_project", None)
+        resource.pop("_rev", None)
+        resource.pop("_deprecated", None)
+        resource.pop("_createdAt", None)
+        resource.pop("_createdBy", None)
+        resource.pop("_updatedAt", None)
+        resource.pop("_updatedBy", None)
+        resource.pop("_incoming", None)
+        resource.pop("_outgoing", None)
+        resource.pop("_constrainedBy", None)
+
+        # write the resource payload as a json file
+        f = open(args.out, "w+")
+        f.write(json.dumps(resource, indent=2))
+        f.close()
+
+        exit(0)
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
